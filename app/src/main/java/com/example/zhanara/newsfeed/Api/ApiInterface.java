27bfd8fe9d07df316be2d5b44bc257d00209c0b9
ZhanarTanirbergen <zhanara.tanirbergenova@gmail.com>
2017-10-13 17:14:53 +0600

    HTTP GET REQUEST

---
+++ app/src/main/java/com/example/zhanara/newsfeed/Api/ApiInterface.java
@@ -0,0 +1,8 @@
+package com.example.zhanara.newsfeed.Api;
+
+/**
+ * Created by zhanara on 12.10.17.
+ */
+
+public class ApiInterface {
+}
---
+++ app/src/main/java/com/example/zhanara/newsfeed/Api/Example.java
@@ -0,0 +1,69 @@
+package com.example.zhanara.newsfeed.ApiModels;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+        "source",
+        "sortBy",
+        "articles"
+})
+public class Example {
+
+    @JsonProperty("source")
+    private String source;
+    @JsonProperty("sortBy")
+    private String sortBy;
+    @JsonProperty("articles")
+    private List<Article> articles = null;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    @JsonProperty("source")
+    public String getSource() {
+        return source;
+    }
+
+    @JsonProperty("source")
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    @JsonProperty("sortBy")
+    public String getSortBy() {
+        return sortBy;
+    }
+
+    @JsonProperty("sortBy")
+    public void setSortBy(String sortBy) {
+        this.sortBy = sortBy;
+    }
+
+    @JsonProperty("articles")
+    public List<Article> getArticles() {
+        return articles;
+    }
+
+    @JsonProperty("articles")
+    public void setArticles(List<Article> articles) {
+        this.articles = articles;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+}
